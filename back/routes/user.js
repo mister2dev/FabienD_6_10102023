d5345010c0ac911f2ffffd4d40ccd1bec28eeea6
mister2dev <michelmaineime@hotmail.com>
2023-12-22 12:35:48 +0100

    like à terminer

--- back/routes/user.js
+++ back/routes/user.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 
+const checkPassword = require('../middleware/password-validator');
+const checkEmail = require("../middleware/email-validator");
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', checkEmail, checkPassword, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
