1319a2bd6f4e77b27c632621d3536d300eb3a4f7
mister2dev <michelmaineime@hotmail.com>
2024-01-23 18:45:33 +0100

    like terminé

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -9,7 +9,11 @@ exports.createSauce = (req, res, next) => {
     const sauce = new Sauce({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
-    });
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [' '],
+        usersdisLiked: [' '],
+        });
     sauce.save()
         .then(() => res.status(201).json({ message: "Sauce enregistré !" }))
         .catch((error) => res.status(400).json({ error }));
