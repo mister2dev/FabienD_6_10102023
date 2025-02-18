bafc8c51612132365230f119e08dd3093297da2a
mister2dev <michelmaineime@hotmail.com>
2025-02-18 01:01:12 +0100

    version deploy

--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, tap } from 'rxjs';
 import { Router } from '@angular/router';
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,13 @@ export class AuthService {
   isAuth$ = new BehaviorSubject<boolean>(false);
   private authToken = '';
   private userId = '';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient,
               private router: Router) {}
 
   createUser(email: string, password: string) {
-    return this.http.post<{ message: string }>('http://localhost:3000/api/auth/signup', {email: email, password: password});
+    return this.http.post<{ message: string }>(`${this.apiUrl}/auth/signup`, {email: email, password: password});
   }
 
   getToken() {
@@ -28,7 +30,7 @@ export class AuthService {
   }
 
   loginUser(email: string, password: string) {
-    return this.http.post<{ userId: string, token: string }>('http://localhost:3000/api/auth/login', {email: email, password: password}).pipe(
+    return this.http.post<{ userId: string, token: string }>(`${this.apiUrl}/auth/login`, {email: email, password: password}).pipe(
       tap(({ userId, token }) => {
         this.userId = userId;
         this.authToken = token;
--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://fabiend-6-10102023.onrender.com/api'
 };
