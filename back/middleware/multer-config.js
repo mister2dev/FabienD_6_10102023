ddd759a5c2395d7f0f96113cdf2ca581293719ce
mister2dev <h2gveille@gmail.com>
2025-02-19 21:07:30 +0100

    version deploy +

--- back/middleware/multer-config.js
+++ back/middleware/multer-config.js
@@ -1,20 +1,38 @@
+// const multer = require("multer");
+
+// const MIME_TYPES = {
+//   "image/jpg": "jpg",
+//   "image/jpeg": "jpg",
+//   "image/png": "png",
+// };
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, "images"); // nom du dossier de destination
+//   },
+//   filename: (req, file, callback) => {
+//     const name = file.originalname.split(" ").join("_"); // remplacement des espaces par des _
+//     const extension = MIME_TYPES[file.mimetype];
+//     callback(null, name + Date.now() + "." + extension); // Nom de sortie du fichier
+//   },
+// });
+
+// module.exports = multer({ storage: storage }).single("image");
+
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../cloudinaryConfig");
 
-const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "images"); // nom du dossier de destination
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_"); // remplacement des espaces par des _
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension); // Nom de sortie du fichier
+// Définir le stockage sur Cloudinary
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "hot-takes", // Dossier où stocker les images sur Cloudinary
+    allowed_formats: ["jpg", "png", "jpeg", "webp"], // Formats autorisés
+    transformation: [{ width: 500, height: 500, crop: "limit" }], // Redimensionnement optionnel
   },
 });
 
-module.exports = multer({ storage: storage }).single("image");
+// Initialisation de Multer
+
+module.exports = multer({ storage: storage }).single("image")
