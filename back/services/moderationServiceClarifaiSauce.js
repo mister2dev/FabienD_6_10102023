1da8c2fee8d17665fc8c134249929adb79024338
mister2dev <michelmaineime@hotmail.com>
2025-03-11 23:04:55 +0100

    maj

--- back/services/moderationServiceClarifaiSauce.js
+++ back/services/moderationServiceClarifaiSauce.js
@@ -2,7 +2,7 @@ const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
 
 const stub = ClarifaiStub.grpc();
 const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key process.env.CLARIFAI_API_KEY");
+metadata.set("authorization", `Key ${process.env.CLARIFAI_API_KEY}`);
 
 // Fonction de validation d'image
 async function validateSauceImage(imageUrl) {
